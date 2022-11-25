afa08e1cfe6b9c1a956d9598d4d61b743ca1bcf7
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-25 10:23:36 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define function expression called divide
+const divide = (num1, num2) => 2000/100;
+isNaN(parseInt(2000, 100));
+
+// Square of number
+const square = num3 => num3*num3;
+square(2)
+// Take two parameters and calculate their sum 
+const add = (a, b)=> a+b;
+add()
