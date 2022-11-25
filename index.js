51d3849a26ba17c7b3d2357b9332b50531b4d027
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-25 10:24:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Define function expression called divide
 const divide = (num1, num2) => 2000/100;
-isNaN(parseInt(2000, 100));
+isNaN(parseInt());
 
 // Square of number
 const square = num3 => num3*num3;
